617b9075ce65736ef8373724b49470e4e92e848d
Kevin Lee <jhssttj@hotmail.com>
2023-01-02 14:29:59 -0700

    Add row display

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,17 +6,26 @@ import '../styles/Board.css';
 
 
 function Board() {
+  //Intialize Board X and Y Size (Played on 15x15)
+  const x = 15;
+  const y= 15;
 
-  //Initalize Board Array (Played on 15x15)
-  const boardArr = initializeBoard(15,15);
+  //Initalize Board Array
+  const boardArr = initializeBoard(x,y);
 
-
+  //Intialize Board Rows
+  const rows = [];
+  for (let i=0; i<x; i++) {
+    rows.push(<div className ="row"></div>)
+  };
 
   //Set Board To Empty Array 
   const [board, setBoard] = useState(boardArr);
 
+
   return (
     <div className= "board">
+      {rows}
     </div>
   );
 }
--- src/helpers/BoardHelper.js
+++ src/helpers/BoardHelper.js
@@ -1,9 +1,9 @@
 //Function to initialize board Array x by y size
 const initializeBoard = (x,y) => {
   let array = [];
-  for (let i = 0; i < y; i++) {
+  for (let i=0; i<y; i++) {
     let tempArray = [];
-    for (let j = 0; j < x; j++) {
+    for (let j=0; j<x; j++) {
       tempArray.push("");
     }
     array.push(tempArray);
