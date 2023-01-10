7eb9f0490004d983f2552e238d934132773d0a45
Kevin Lee <jhssttj@hotmail.com>
2023-01-10 14:21:08 -0700

    Add hover state for stones

--- src/components/BoardSquare.js
+++ src/components/BoardSquare.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useState} from 'react';
 import '../styles/Board.css';
 import piece1 from '../images/piece-black.png'
 import piece2 from '../images/piece-white.png'
@@ -12,12 +13,29 @@ function BoardSquare(props) {
   const board = props.board;
   //Find if piece has already been placed or not and disable onclick if so
   const placed = board[y][x] !== "";
+  //Set hover state to render pre-placed stone location
+  const [hover, setHover] = useState(false);
+  //Function to check hover
+  const isHover = () => {
+    if (hover) {
+      setHover(false);
+      console.log("Hover off");
+      return;
+    }
+    setHover(true);
+    console.log("Hover on");
+    return;
+  }
   return (
     <div 
     className={className} 
     x={x} 
     y={y} 
-    onClick={placed? ()=>{} : (event)=>{placeStone(event)}}>
+    onClick={placed? ()=>{} : (event)=>placeStone(event)}
+    onMouseEnter={()=>setHover(true)}
+    onMouseLeave={()=>setHover(false)}
+    >
+      {hover && <h1>hovered</h1>}
       {board[y][x] === 1 &&
         <img src={piece1} alt='p1' className='p1 stone'/>
       }
