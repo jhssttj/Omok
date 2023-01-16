c69310a3d97ab346c2800e9743d772b7290dc8b2
Kevin Lee <jhssttj@hotmail.com>
2023-01-15 17:32:06 -0700

    Add function to change between pieces

--- src/components/MainScreen/PlayerPieceScreen.js
+++ src/components/MainScreen/PlayerPieceScreen.js
@@ -4,27 +4,22 @@ import {pieces} from './PlayerData';
 import { FaRegArrowAltCircleLeft,FaRegArrowAltCircleRight } from "react-icons/fa";
 
 function PlayerPieceScreen(props) {
-  const [current, setCurrent] =useState(props.player===1?pieces[0]:pieces[1]);
+  const [current, setCurrent] =useState(props.player===1?0:1);
 
   //Function to change between pieces
-  const changePiece = () => {
-    console.log("Hi");
+  const nextPiece = () => {
+    setCurrent(current===pieces.length-1?0:current+1);
+  }
+  const prevPiece = () => {
+    setCurrent(current===0?pieces.length-1:current-1);
   }
 
-  //Render all piece image
-  const pieceRender = pieces.map((image,index) => {
-    return (
-      <div key={index}>
-        <img src={image} alt='pieceImage'/>
-      </div>
-    )
-  })
 
   return (
     <div>
-      <FaRegArrowAltCircleLeft className="button left" onClick={changePiece}/>
-      <FaRegArrowAltCircleRight className="button left" onClick={changePiece}/>
-      {pieceRender}
+      <FaRegArrowAltCircleLeft className="button left" onClick={prevPiece}/>
+      <img src={pieces[current]} alt='pieceImage'/>
+      <FaRegArrowAltCircleRight className="button left" onClick={nextPiece}/>
     </div>
   );
 }
